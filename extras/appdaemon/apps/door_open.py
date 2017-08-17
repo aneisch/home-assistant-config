b9b18ec38568170b50dd29f3d0de04618b90907b
Andrew <andrew@aneis.ch>
2017-08-17 11:48:06 -0500

    Updated door notify app

--- extras/appdaemon/apps/door_open.py
+++ extras/appdaemon/apps/door_open.py
@@ -29,6 +29,8 @@ class DoorNotify(appapi.AppDaemon):
 
   def state_change(self, entity, attribute, old, new, kwargs):
     if entity == "sensor.front_door_status" and new == "Open" and self.get_state("input_boolean.door_notify") == "on":
-      self.call_service("notify/notify_push_android", title = "Home Assistant", message = "Front Door Opened")
+      self.log(entity + "changed to opened, notifying")
+      self.call_service("notify/" + self.args["notify"], title = "Home Assistant", message = "Front Door Opened")
     elif entity == "sensor.back_door_status" and new == "Open" and self.get_state("input_boolean.door_notify") == "on":
-      self.call_service("notify/notify_push_android", title = "Home Assistant", message = "Back Door Opened")
+      self.log(entity + "changed to opened, notifying")
+      self.call_service("notify/" + self.args["notify"], title = "Home Assistant", message = "Back Door Opened")
