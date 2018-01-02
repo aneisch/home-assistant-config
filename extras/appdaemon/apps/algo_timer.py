a688a61415252d564d10cf71c232773ab392d87e
Andrew <andrew@aneis.ch>
2018-01-02 13:34:49 -0600

    Updated.

--- extras/appdaemon/apps/algo_timer.py
+++ extras/appdaemon/apps/algo_timer.py
@@ -1,7 +1,6 @@
 import appdaemon.appapi as appapi, sqlite3
 from datetime import datetime, timedelta
 
-
 class SmartTimer(appapi.AppDaemon):
   def initialize(self):
     
@@ -15,23 +14,24 @@ class SmartTimer(appapi.AppDaemon):
 
   # Calculate the average amount of time a switch is on
   def calculate_times(self, entity, attribute, old, new, kwargs):
-    
+
     # Only execute if turned on
     if new == "on":
-      
+
       #Calculate past lengths of time
       conn = sqlite3.connect("file:" +  kwargs["db_location"] + "?mode=ro", uri=True)
       c = conn.cursor()
       c.execute('SELECT state,last_changed FROM states where entity_id = "' + self.args["entity_id"] + '"')
-      
+
       results = c.fetchall()
       conn.close()
       if self.args["debug"]:
         self.log("Entity changed.. reprocessing times")
-      
+
       records = 0
       times = []
-      
+
+      # Iterate through entity changed times and create a list of times spent "on"
       for result in results:
         if self.args["debug"]:
           self.log(result)
@@ -42,23 +42,28 @@ class SmartTimer(appapi.AppDaemon):
           off = result[1]
           process = True
         if process == True:
+          # Takes "off" timestamp and subtracts "on" timestamp to find timedelta
           length = datetime.strptime(off, '%Y-%m-%d %H:%M:%S.%f') - datetime.strptime(on, '%Y-%m-%d %H:%M:%S.%f')
           if self.args["debug"]:
             self.log("Found length: " + str(length))
           times.append(length)
           records += 1
-          
+
+      # Calculate average time spent "on"
       average = sum(times, timedelta(0)) / len(times)
+      # Get total seconds from datetime hours, minutes, seconds
       average_seconds = int(timedelta.total_seconds(average))
-      
+
       if self.args["debug"]:
         self.log("Calculation complete, determined average on time of " + str(average))
-      
+
+
+      # Schedule our turn_off action in X seconds in the future, the average time entity has spent is "on"
       self.log("Scheduling turn_off of %s in %s seconds." % (self.args["entity_id"],str(average_seconds)))
-        
       self.run_in(self.average_exceeded, average_seconds)
 
 
   def average_exceeded(self, kwargs):
       self.log("Average time exceeded, turning off %s" % self.args["entity_id"])
+      # Turn off specified entity
       self.turn_off(self.args["entity_id"]) 
