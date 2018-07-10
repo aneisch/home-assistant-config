36dd1cc5b419c94f80415111e963879a2c8a4ea9
Andrew <andrew@aneis.ch>
2018-07-09 22:09:58 -0500

    Appdaemon app updated. Lamp automation moved to AppDaemon

--- extras/appdaemon/apps/light_automation.py
+++ extras/appdaemon/apps/light_automation.py
@@ -2,14 +2,23 @@ import appdaemon.plugins.hass.hassapi as hass
 
 
 #Turn on a scene at given time (specified in apps.yaml) 
-class SceneTime(hass.Hass):
+class DeviceOn(hass.Hass):
   def initialize(self):
     time_on = self.parse_time(self.args["time_on"])
     self.run_daily(self.on, time_on)
 
   def on(self, kwargs):
-    if self.get_state(self.args["device_tracker"]) == "home" and self.get_state(self.args["dependent_light"]) == "on":
-      self.log(self.args["scene"])
-      self.turn_on(self.args["scene"])
+    if self.args["dependent_entity"]:
+      if self.get_state(self.args["device_tracker"]) == "home" and self.get_state(self.args["dependent_entity"]) == "on":
+        for entity in self.split_device_list(self.args["entity"]):
+          self.log("Turning " + entity + " on")
+          self.turn_on(entity)
+      else:
+        pass
     else:
-      pass
+      if self.get_state(self.args["device_tracker"]) == "home":
+        for entity in self.split_device_list(self.args["entity"]):
+          self.log("Turning " + entity + " on")
+          self.turn_on(entity)
+      else:
+        pass
