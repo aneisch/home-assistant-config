84833ce4a36e1e14cdd9bfd98e492261ca29d2d7
Andrew <andrew@aneis.ch>
2018-10-23 20:08:50 -0500

    Probably broke all my appdaemon apps

--- extras/appdaemon/apps/door_open.py
+++ extras/appdaemon/apps/door_open.py
@@ -2,37 +2,35 @@ import appdaemon.plugins.hass.hassapi as hass
 
 
 class DoorLight(hass.Hass):
-  def initialize(self):
-    if "door_sensor" in self.args:
-      for sensor in self.split_device_list(self.args["door_sensor"]):
-        self.listen_state(self.state_change, sensor)
+    def initialize(self):
+        if "door_sensor" in self.args:
+            for sensor in self.split_device_list(self.args["door_sensor"]):
+                self.listen_state(self.state_change, sensor)
 
-  def state_change(self, entity, attribute, old, new, kwargs):
-    for light in self.split_device_list(self.args["lights"]):
-      if new == "on" and self.get_state(light) == "off":
-        self.log("Turning " + light + " On")
-        self.turn_on(light)
-        self.run_in(self.light_off, self.args["time_on"], switch=light)
+    def state_change(self, entity, attribute, old, new, kwargs):
+        for device in self.split_device_list(self.args["lights"]):
+            if new == "on" and self.get_state(device) == "off":
+                self.log("Turning " + device + " On")
+                self.turn_on(device)
+                # Schedule the turn off
+                self.run_in(self.light_off, self.args["time_on"], device=device)
 
-  def light_off(self, args):
-    self.log("Turning " + args["switch"] + " Off")
-    self.turn_off(args["switch"])
+    def light_off(self, args):
+        self.log("Turning " + args["device"] + " Off")
+        self.turn_off(args["device"])
 
 
 class DoorNotify(hass.Hass):
-  def initialize(self):
-    if "door_sensor" in self.args:
-      for sensor in self.split_device_list(self.args["door_sensor"]):
-        self.listen_state(self.state_change, sensor)
+    def initialize(self):
+        if "door_sensor" in self.args:
+            for sensor in self.split_device_list(self.args["door_sensor"]):
+                self.listen_state(self.state_change, sensor)
 
-  def state_change(self, entity, attribute, old, new, kwargs):
+    def state_change(self, entity, attribute, old, new, kwargs):
+        if entity == "binary_sensor.front_door" and new == "on":
+            self.log(entity + "changed to opened, notifying")
+            self.call_service("notify/" + self.args["notify"], title = "Home Assistant", message = "Front Door Opened")
 
-    if self.get_state("input_boolean.door_notify") == "on":
-
-      if entity == "binary_sensor.front_door" and new == "on":
-        self.log(entity + "changed to opened, notifying")
-        self.call_service("notify/" + self.args["notify"], title = "Home Assistant", message = "Front Door Opened")
-
-      elif entity == "binary_sensor.back_door" and new == "on":
-        self.log(entity + "changed to opened, notifying")
-        self.call_service("notify/" + self.args["notify"], title = "Home Assistant", message = "Back Door Opened")
+        elif entity == "binary_sensor.back_door" and new == "on":
+            self.log(entity + "changed to opened, notifying")
+            self.call_service("notify/" + self.args["notify"], title = "Home Assistant", message = "Back Door Opened")
--- extras/appdaemon/apps/light_automation.py
+++ extras/appdaemon/apps/light_automation.py
@@ -1,25 +1,20 @@
 import appdaemon.plugins.hass.hassapi as hass
 
 
-#Turn on a scene at given time (specified in apps.yaml) 
+#Turn on a scene at given time (specified in apps.yaml)
 class DeviceOn(hass.Hass):
   def initialize(self):
     time_on = self.parse_time(self.args["time_on"])
     self.run_daily(self.on, time_on)
 
-  def on(self, kwargs):
-    self.log("DEBUG: DeviceOn triggered")
-    if self.args["dependent_entity"]:
-      if self.get_state(self.args["device_tracker"]) == "home" and self.get_state(self.args["dependent_entity"]) == "on":
-        for entity in self.split_device_list(self.args["entity"]):
-          self.log("Turning " + entity + " on")
-          self.turn_on(entity)
-      else:
-        pass
-    else:
-      if self.get_state(self.args["device_tracker"]) == "home":
-        for entity in self.split_device_list(self.args["entity"]):
-          self.log("Turning " + entity + " on")
-          self.turn_on(entity)
-      else:
-        pass
+    def on(self, kwargs):
+        if self.args["dependent_entity"]:
+            if self.get_state(self.args["dependent_entity"]) == "on":
+                for entity in self.split_device_list(self.args["entity"]):
+                    self.log("Turning " + entity + " on")
+                    self.turn_on(entity)
+        else:
+            if self.get_state(self.args["device_tracker"]) == "home":
+                for entity in self.split_device_list(self.args["entity"]):
+                    self.log("Turning " + entity + " on")
+                    self.turn_on(entity)
