dac6deb14a745abd0e212deca4dc7a378afb1bc3
Andrew <andrewneisch@gmail.com>
2019-08-20 15:39:09 -0500

    Modify date reminder to use input_datetime

--- custom_components/date_reminder/sensor.py
+++ custom_components/date_reminder/sensor.py
@@ -23,13 +23,13 @@ ATTR_DAYS = 'days_since'
 ATTR_REMAINING = 'days_remaining'
 
 DEFAULT_NAME = 'Date Reminder'
-CONF_START_DATE = 'start_date'
+CONF_START_INPUT_DATETIME = 'start_date_input'
 CONF_REMINDER = 'days_in_future'
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=60)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_START_DATE): cv.string,
+    vol.Required(CONF_START_INPUT_DATETIME): cv.string,
     vol.Required(CONF_REMINDER): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
 })
@@ -37,19 +37,20 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up date countdown sensor."""
-    start_date = config.get(CONF_START_DATE)
     days_in_future = config.get(CONF_REMINDER)
     sensor_name = config.get(CONF_NAME)
+    input_datetime = config.get(CONF_START_INPUT_DATETIME)
 
-    add_devices([Reminder(sensor_name, start_date, days_in_future)])
+    add_devices([Reminder(hass, input_datetime, sensor_name, days_in_future)])
 
 
 class Reminder(Entity):
     """Implementation of the date reminder sensor."""
 
-    def __init__(self, sensor_name, start_date, days_in_future):
+    def __init__(self, hass, input_datetime, sensor_name, days_in_future):
         """Initialize the sensor."""
-        self.start_date = start_date
+        self._input_datetime = input_datetime
+        self.hass = hass
         self.days_in_future = days_in_future
         self._name = sensor_name
         self._state = None
@@ -81,7 +82,8 @@ class Reminder(Entity):
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """Calculate time since start"""
-        start_date = datetime.datetime.strptime(self.start_date, '%d-%m-%Y %H:%M')
+        start_date = self.hass.states.get(self._input_datetime).state
+        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
         days = (datetime.datetime.now() - start_date)
 
         days = days.days
