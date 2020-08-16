8c9687c57d71fe4c8c8b5ff5cd98ad339ceb226c
Andrew <andrewneisch@gmail.com>
2020-08-15 23:16:20 -0500

    Appdaemon tweaks

--- extras/appdaemon/apps/light_automation.py
+++ extras/appdaemon/apps/light_automation.py
@@ -1,8 +1,9 @@
-import appdaemon.plugins.hass.hassapi as hass
+import hassapi as hass
 
 #Turn on a scene at given time (specified in apps.yaml)
 class DeviceOn(hass.Hass):
     def initialize(self):
+      self.log(self.list_constraints())
       time_on = self.parse_time(self.args["time_on"])
       self.run_daily(self.on, time_on)
 
