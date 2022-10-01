810e8fad15633b4f0c948320ee0ff46a099ee693
Andrew Neisch <andrew@aneis.ch>
2022-10-01 15:45:24 -0500

    Updates, Beta

--- custom_components/sonoff/core/devices.py
+++ custom_components/sonoff/core/devices.py
@@ -14,13 +14,13 @@ Developer can change global properties of existing classes via spec function.
 from .ewelink import XDevice
 from ..binary_sensor import *
 from ..climate import XClimateTH, XClimateNS, XThermostat
-from ..cover import XCover, XCoverDualR3
+from ..cover import XCover, XCoverDualR3, XZigbeeCover
 from ..fan import XFan, XDiffuserFan, XToggleFan
 from ..light import *
+from ..number import XPulseWidth
 from ..remote import XRemote
 from ..sensor import *
 from ..switch import *
-from ..number import XPulseWidth
 
 # supported custom device_class
 DEVICE_CLASS = {
@@ -125,7 +125,10 @@ DEVICES = {
     ],
     22: [XLightB1, RSSI],  # Sonoff B1 (only cloud)
     # https://github.com/AlexxIT/SonoffLAN/issues/173
-    25: [XDiffuserFan, XDiffuserLight, XWater, RSSI],  # Diffuser
+    25: [
+        XDiffuserFan, XDiffuserLight, RSSI,
+        spec(XBinarySensor, param="water", uid=""),
+    ],  # Diffuser
     28: [XRemote, LED, RSSI],  # Sonoff RF Brigde 433
     29: SPEC_2CH,
     30: SPEC_3CH,
@@ -138,6 +141,7 @@ DEVICES = {
         spec(XEnergySensor, param="hundredDaysKwhData", uid="energy",
              get_params={"hundredDaysKwh": "get"}),
     ],  # Sonoff POWR2
+    33: [XLightL1, RSSI],  # https://github.com/AlexxIT/SonoffLAN/issues/985
     34: [
         XFan, XFanLight, LED, RSSI,
     ],  # Sonoff iFan02 and iFan03
@@ -145,7 +149,7 @@ DEVICES = {
     44: [XLightD1, RSSI],  # Sonoff D1
     57: [XLight57, RSSI],  # Mosquito Killer Lamp
     59: [XLightL1, RSSI],  # Sonoff LED (only cloud)
-    # 66: switch1,  # ZigBee Bridge
+    66: [RSSI],  # ZigBee Bridge
     77: SPEC_1CH,  # Sonoff Micro
     78: SPEC_1CH,  # https://github.com/AlexxIT/SonoffLAN/issues/615
     81: SPEC_1CH,
@@ -175,8 +179,12 @@ DEVICES = {
     # https://github.com/AlexxIT/SonoffLAN/issues/766
     136: [XLightB05B, RSSI],  # Sonoff B05-BL
     137: [XLightL1, RSSI],
+    # https://github.com/AlexxIT/SonoffLAN/issues/808
+    154: [XWiFiDoor, Battery, RSSI],  # DW2-Wi-Fi-L
     162: SPEC_3CH,  # https://github.com/AlexxIT/SonoffLAN/issues/659
     165: [Switch1, Switch2, RSSI],  # DualR3 Lite, without power consumption
+    # https://github.com/AlexxIT/SonoffLAN/issues/857
+    168: [RSSI],  # new ZBBridge-P
     174: [XRemoteButton],  # Sonoff R5 (6-key remote)
     177: [XRemoteButton],  # Sonoff S-Mate
     181: [
@@ -198,13 +206,24 @@ DEVICES = {
     ],  # Sonoff POWR3
     1000: [XRemoteButton, Battery],  # zigbee_ON_OFF_SWITCH_1000
     1256: [spec(XSwitch, base="light")],  # ZCL_HA_DEVICEID_ON_OFF_LIGHT
+    # https://github.com/AlexxIT/SonoffLAN/issues/972
+    1514: [XZigbeeCover, spec(XSensor, param="battery", multiply=2)],
     1770: [
         spec(XSensor100, param="temperature"),
         spec(XSensor100, param="humidity"),
         Battery,
     ],  # ZCL_HA_DEVICEID_TEMPERATURE_SENSOR
     2026: [XZigbeeMotion, Battery],  # ZIGBEE_MOBILE_SENSOR
-    3026: [XZigbeeDoor, Battery],  # ZIGBEE_DOOR_AND_WINDOW_SENSOR
+    # ZIGBEE_DOOR_AND_WINDOW_SENSOR
+    3026: [
+        # backward compatibility for unique_id
+        spec(XBinarySensor, param="lock", uid="", default_class="door"),
+        Battery,
+    ],
+    4026: [
+        spec(XBinarySensor, param="water", uid="", default_class="moisture"),
+        Battery,
+    ],  # https://github.com/AlexxIT/SonoffLAN/issues/852
     4256: [
         spec(XZigbeeSwitches, channel=0, uid="1"),
         spec(XZigbeeSwitches, channel=1, uid="2"),
@@ -224,6 +243,8 @@ POW_UI_ACTIVE = {
     126: (3600, {"uiActive": {"all": 1, "time": 7200}}),
     130: (3600, {"uiActive": {"all": 1, "time": 7200}}),
     182: (0, {"uiActive": 180}),  # maximum for this model
+    # https://github.com/AlexxIT/SonoffLAN/issues/978
+    190: (0, {"uiActive": 180}),  # haven't check real maximum
 }
 
 
@@ -330,6 +351,7 @@ DIY = {
     "rf": [28, "SONOFF", "RFBridge DIY"],
     "fan_light": [34, "SONOFF", "iFan DIY"],
     "light": [44, "SONOFF", "D1 DIY"],
+    "switch_radar": [77, "SONOFF", "Micro DIY"],
     "multifun_switch": [126, "SONOFF", "DualR3 DIY"],
 }
 
