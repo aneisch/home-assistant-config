efce7a88586834211db592bca78e7a40b31a7929
Andrew Neisch <andrew@aneis.ch>
2023-10-07 16:46:15 -0500

    Changes

--- custom_components/mail_and_packages/const.py
+++ custom_components/mail_and_packages/const.py
@@ -12,7 +12,7 @@ from homeassistant.helpers.entity import EntityCategory
 
 DOMAIN = "mail_and_packages"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.3.18"
+VERSION = "0.3.19"
 ISSUE_URL = "http://github.com/moralmunky/Home-Assistant-Mail-And-Packages"
 PLATFORM = "sensor"
 PLATFORMS = ["binary_sensor", "camera", "sensor"]
