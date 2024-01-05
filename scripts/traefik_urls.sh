fc1b44f46d4675d50b8d05f363603b9a7e502942
Andrew Neisch <andrew@aneis.ch>
2024-01-04 22:35:46 -0600

    Comments

--- scripts/traefik_urls.sh
+++ scripts/traefik_urls.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Read active routers from Traefik API and output JSON of all host rules
+
 IFS=, read -d '' -a myarray <<< $(curl --silent http://traefik.home.mydomain.com/api/http/routers | jq -r . | grep -i rule | sed -e 's/.*ost(`//' -e 's/`.*//')
 
 json_array=$(jq --compact-output --null-input '$ARGS.positional' --args -- ${myarray[@]})
