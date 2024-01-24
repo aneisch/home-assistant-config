509a641f2e32cb44c9411f1994b7311aceba4cd9
Andrew Neisch <andrew@aneis.ch>
2024-01-23 23:17:24 -0600

    Updates

--- extras/appdaemon/apps/alexa_timer_sync.py
+++ extras/appdaemon/apps/alexa_timer_sync.py
@@ -17,9 +17,6 @@ class TimerSync(hass.Hass):
         new = json.loads(new)
         count = 0
 
-        self.log(new)
-        self.log(count)
-
         for i in range(0,6):
             self.call_service("timer/cancel", entity_id = f"timer.{self.timer_prefix}_{i}")
 
@@ -30,14 +27,22 @@ class TimerSync(hass.Hass):
             if label == "":
                 label = "No Name"
             trigger_time = int(timer['triggerTime']/1000)
+            created_time = int(timer['createdDate']/1000)
+            timer_id = timer['id'][-8:] # Just grab the last 8 digits of the ID
 
-            self.log(f"{label}: {trigger_time}")
-
+            # Calculate original length in h:m:s
+            millis = int(timer['originalDurationInMillis'])
+            seconds=int(millis/1000)%60
+            minutes=int(millis/(1000*60))%60
+            hours=int(millis/(1000*60*60))%24
+            pretty_length =  f"{hours}h {minutes}m {seconds}s"
             current_time = datetime.fromtimestamp(time.time())
             trigger_time = datetime.fromtimestamp(trigger_time)
+            created_time = datetime.fromtimestamp(created_time)
             duration = str(trigger_time - current_time).split(".")[0]
+
             if "-" not in str(duration):
-                self.log(f"now: {current_time} end: {trigger_time} length: {duration}")
+                self.log(f"{label} - ID: {timer_id} - Created: {created_time} - Start: {current_time} - End: {trigger_time} - Original Length: {pretty_length} - Calculated Remaining: {duration}")
                 self.call_service("timer/start", entity_id = f"timer.{self.timer_prefix}_{count}", duration = f"{duration}")
                 self.call_service("input_text/set_value", entity_id = f"input_text.{self.timer_prefix}_{count}_name", value = label)
             else:
