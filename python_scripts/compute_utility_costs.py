59bce31359058d3a084fdb9ea8173775ca69a927
Andrew Neisch <andrew@aneis.ch>
2024-10-05 22:33:08 -0500

    Changes

--- python_scripts/compute_utility_costs.py
+++ python_scripts/compute_utility_costs.py
@@ -1,16 +1,15 @@
 MeterEntity = data.get('meter_entity')
 inputState = hass.states.get(MeterEntity).state
 
+'''
+Electric:
+1. Service charge: $7.00 per month; plus
+2. Energy charge: $0.1187 per kWh for all kWh; plus
+3. Transmission Delivery Adjustment: $0.0175 per kWh.
+'''
+current_cost = 0.1187 + 0.0175
+
 if MeterEntity == "sensor.electricity_usage":
-    '''
-    Electric:
-    1. Service charge: $7.00 per month; plus
-    2. Energy charge: $0.1187 per kWh for all kWh; plus
-    3. Transmission Delivery Adjustment: $0.0175 per kWh.
-    '''
-
-    current_cost = 0.1187 + 0.0175
-
     #hass.bus.fire("debug", {"wow": ""})
 
     # Add $7 monthly charge to summary total
@@ -20,6 +19,13 @@ if MeterEntity == "sensor.electricity_usage":
     service_data = {"entity_id": "input_text.electricity_cost_monthly_emporia", "value": computed_electricity_cost}
     hass.services.call("input_text", "set_value", service_data)
 
+elif MeterEntity == "sensor.tesla_energy_this_month":
+    # Add $7 monthly charge to summary total
+    computed_electricity_cost = round((float(inputState) * current_cost), 2)
+
+    # Set State
+    service_data = {"entity_id": "input_text.electricity_cost_monthly_tesla", "value": computed_electricity_cost}
+    hass.services.call("input_text", "set_value", service_data)
 
 elif MeterEntity == "sensor.water_usage":
     unaccountedusage = int(float(inputState))
